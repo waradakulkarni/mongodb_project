5a2ec9b042d8bb1ff571eebe16a3636928b3c44c
waradakulkarni <94771210+waradakulkarni@users.noreply.github.com>
2022-04-16 14:55:07 -0700

    Update settings.py

--- mongoproject/settings.py
+++ mongoproject/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_COOKIE_SECURE = False
 ROOT_URLCONF = 'mongoproject.urls'
 
 TEMPLATES = [
