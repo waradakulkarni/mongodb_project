20a60a0e29d2afcb749e6769b5f054f60c73d53f
waradakulkarni <94771210+waradakulkarni@users.noreply.github.com>
2022-04-16 14:54:43 -0700

    Add files via upload

---
+++ mongoproject/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+import pymongo
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from pymongo import MongoClient
+
+#uri = 'localhost:27017'
+uri = 'mongodb://54.224.111.187:27017'
+client = MongoClient(uri)
+
+#my_client = pymongo.MongoClient(settings.DB_NAME)
+
+# First define the database name
+dbname = client['dummy']
+
+# Now get/create collection name (remember that you will see the database in your mongodb cluster only after you create a collection)
+business_collection_name = dbname["business"]
+reviews_collection_name = dbname["review"]
+
+def business_top(request):
+
+    dictlist=[]
+    for d in business_collection_name.find({'stars':{'$gt':4}},{'_id':0}).limit(10):
+        dictlist.append(d)
+
+    return HttpResponse(json.dumps(dictlist))
+
+def business_happyhour(request):
+
+    dictlist=[]
+    for d in business_collection_name.find({'attributes.HappyHour' : "True"}, {'_id': 0}).limit(10):
+        dictlist.append(d)
+
+    return HttpResponse(json.dumps(dictlist))
+
+
+def business_isopen(request, id, open):
+
+    business_collection_name.update_one({"business_id": id}, {"$set": {"is_open": int(open)}})
+
+    return HttpResponse(status=200)
+
+def business_delete(request, id):
+
+    business_collection_name.delete_one({"business_id": id})
+
+    reviews_collection_name.delete_many({"business_id": id})
+
+    return HttpResponse(status=200)
+
+@csrf_exempt
+def reviews_insert(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    print(body)
+    #content = body['content']
+    reviews_collection_name.insert_one(body)
+    #find = reviews_collection_name.find_one()
+    #print(content)
+    return HttpResponse(status=200)
