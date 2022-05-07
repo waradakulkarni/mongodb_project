78f6eaa55d8fa09b1aebcb4298453de8968e2f5b
waradakulkarni <94771210+waradakulkarni@users.noreply.github.com>
2022-05-07 16:46:33 -0700

    Update views.py

--- mongoproject/views.py
+++ mongoproject/views.py
@@ -7,7 +7,11 @@ from django.views.decorators.csrf import csrf_exempt
 from pymongo import MongoClient
 
 #uri = 'localhost:27017'
-uri = 'mongodb://54.224.111.187:27017'
+# host = 'localhost'
+# port = 27017
+# client = MongoClient(host, port)
+
+uri = 'mongodb://54.174.38.160:27017'
 client = MongoClient(uri)
 
 #my_client = pymongo.MongoClient(settings.DB_NAME)
@@ -20,7 +24,7 @@ business_collection_name = dbname["business"]
 reviews_collection_name = dbname["review"]
 
 def business_top(request):
-
+    print("In business top")
     dictlist=[]
     for d in business_collection_name.find({'stars':{'$gt':4}},{'_id':0}).limit(10):
         dictlist.append(d)
@@ -34,7 +38,6 @@ def business_happyhour(request):
         dictlist.append(d)
 
     return HttpResponse(json.dumps(dictlist))
-
 
 def business_isopen(request, id, open):
 
@@ -71,6 +74,122 @@ def review_find(request, id):
 
     dictlist = []
     for d in reviews_collection_name.find({"review_id": str(id)}, {'_id': 0}).limit(1):
+        dictlist.append(d)
+
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_find_topten(request, id):
+    dictlist = []
+    cursor = business_collection_name.aggregate(
+        [{"$match": {"postal_code": "85711", "categories": {"$regex": "Restaurants"}}},
+         {"$sort": {"stars": -1, "review_count": -1}},
+         {"$limit": 10}
+         ])
+    for d in cursor:
+        dictlist.append(d)
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_category(request, category):
+
+    dictlist = []
+    for d in business_collection_name.find({"categories":{"$regex":str(category)}}):
+        dictlist.append(d)
+
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_display_timings(request, name):
+
+    dictlist = []
+    for d in business_collection_name.find({"name":str(name), "categories":{ "$regex": "Restaurants" }},{"hours":1,"_id":0}):
+        dictlist.append(d)
+
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_display_latestreview(request, id):
+    dictlist = []
+    cursor = reviews_collection_name.aggregate([{"$match": {"business_id": str(id)}},
+                                                {"$sort": {"date": -1}},
+                                                {"$limit": 1}
+                                                ])
+
+    for d in cursor:
+        dictlist.append(d)
+
+    print(dictlist)
+
+    return HttpResponse(json.dumps(dictlist))
+
+def business_takeout(request):
+
+    dictlist = []
+    for d in business_collection_name.find({'attributes.RestaurantsTakeOut' : "True"},{"_id":0}).limit(10):
+        dictlist.append(d)
+
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_avgrating_city(request, city):
+    dictlist = []
+    cursor = business_collection_name.aggregate(
+        [{"$match": {"city": str(city), "categories": {"$regex": "Restaurants"}}},
+         {"$group": {"_id": "$city", "avg_val": {"$avg": "$stars"}}}
+         ])
+
+    for d in cursor:
+        dictlist.append(d)
+
+    print(dictlist)
+
+    return HttpResponse(json.dumps(dictlist))
+
+def business_avgrating_state(request, state):
+    dictlist = []
+    cursor = business_collection_name.aggregate(
+        [{"$match": {"state": str(state), "categories": {"$regex": "Restaurants"}}},
+         {"$group": {"_id": "state", "avg_val": {"$avg": "$stars"}}}
+         ])
+
+    for d in cursor:
+        dictlist.append(d)
+
+    print(dictlist)
+
+    return HttpResponse(json.dumps(dictlist))
+
+def business_keyword_search(request, keyword):
+
+    dictlist = []
+    for d in business_collection_name.find({"name":{"$regex":str(keyword)}}).limit(10):
+        dictlist.append(d)
+
+    print(dictlist)
+    return HttpResponse(json.dumps(dictlist))
+
+def business_display_longestreview(request, id):
+    dictlist = []
+    cursor = reviews_collection_name.aggregate([{"$match": {"business_id": str(id)}}, {
+        "$addFields": {
+            "length": {"$strLenCP": "$text"}
+        }},
+                                                {"$sort": {"length": -1}},
+                                                {"$limit": 1}
+                                                ])
+
+    for d in cursor:
+        dictlist.append(d)
+
+    print(dictlist)
+
+    return HttpResponse(json.dumps(dictlist))
+
+def business_openday(request):
+
+    dictlist = []
+    for d in business_collection_name.find({"hours.Monday": {"$exists":True}, "categories":{ "$regex": "Restaurants" }}).limit(10):
         dictlist.append(d)
 
     print(dictlist)
